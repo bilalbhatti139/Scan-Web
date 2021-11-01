26ebd6db124baff1c40b2c244d865f14741e9d71
bilalbhatti139 <bilalbhatti139@gmail.com>
2021-11-01 20:01:32 +0500

    features completed

--- src/components/features/index.js
+++ src/components/features/index.js
@@ -23,7 +23,7 @@ const Features = () => {
           <Container>
             <div className="features-wrapper__container">
               <div className="features-wrapper__content-box">
-                <div className="features-wrapper__img">
+                <div className="features-wrapper__image">
                   <img
                     src={EC}
                     alt="text-logo"
@@ -31,11 +31,14 @@ const Features = () => {
                     width="150"
                   />
                 </div>
-                <h4>Easy Control</h4>
-                <p>ease in managing and viewing your finances</p>
+                <h4 className="features-wrapper__box-title">Easy Control</h4>
+                <hr className="features-wrapper__underline__one" />
+                <p className="features-wrapper__body">
+                  ease in managing and viewing your finances
+                </p>
               </div>
               <div className="features-wrapper__content-box">
-                <div className="features-wrapper__img">
+                <div className="features-wrapper__image">
                   <img
                     src={FS}
                     alt="text-logo"
@@ -43,11 +46,16 @@ const Features = () => {
                     width="150"
                   />
                 </div>
-                <h4>Financial Stability</h4>
-                <p>Your finance stability is 90% improved with our help</p>
+                <h4 className="features-wrapper__box-title">
+                  Financial Stability
+                </h4>
+                <hr className="features-wrapper__underline__two" />
+                <p className="features-wrapper__body">
+                  Your finance stability is 90% improved with our help
+                </p>
               </div>
               <div className="features-wrapper__content-box">
-                <div className="features-wrapper__img">
+                <div className="features-wrapper__image">
                   <img
                     src={TC}
                     alt="text-logo"
@@ -55,8 +63,11 @@ const Features = () => {
                     width="150"
                   />
                 </div>
-                <h4>Total control</h4>
-                <p>To maintain your comfort in using our features</p>
+                <h4 className="features-wrapper__box-title">Total control</h4>
+                <hr className="features-wrapper__underline__three" />
+                <p className="features-wrapper__body">
+                  To maintain your comfort in using our features
+                </p>
               </div>
             </div>
           </Container>
