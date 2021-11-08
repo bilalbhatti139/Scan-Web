2acd2183e21dd6a80d9976c77e645a1e2af07e03
bilalbhatti139 <bilalbhatti139@gmail.com>
2021-11-09 00:05:46 +0500

    animation

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,50 +2,62 @@ const benefits = [
   {
     image: "/images/secure.png",
     title: "Secure",
+    back: "/images/secure.png",
   },
   {
     image: "/images/productive.png",
     title: "Productive",
+    back: "/images/secure.png",
   },
   {
     image: "/images/safe.png",
     title: "Safe",
+    back: "/images/secure.png",
   },
   {
     image: "/images/ease-of-use.png",
     title: "Ease of Use",
+    back: "/images/secure.png",
   },
   {
     image: "/images/simple.png",
     title: "simple",
+    back: "/images/secure.png",
   },
   {
     image: "/images/futuristic.png",
     title: "Futuristic",
+    back: "/images/secure.png",
   },
   {
     image: "/images/research.png",
     title: "Research",
+    back: "/images/secure.png",
   },
   {
     image: "/images/privacy.png",
     title: "Privacy",
+    back: "/images/secure.png",
   },
   {
     image: "/images/quality.png",
     title: "Quality",
+    back: "/images/secure.png",
   },
   {
     image: "/images/scalable.png",
     title: "Scalable",
+    back: "/images/secure.png",
   },
   {
     image: "/images/fast.png",
     title: "Fast",
+    back: "/images/secure.png",
   },
   {
     image: "/images/efficient.png",
     title: "Efficient",
+    back: "/images/secure.png",
   },
 ];
 
